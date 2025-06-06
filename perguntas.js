ca414600b83b93cffcb7b72a320ddb49b56975c8
Nikixcz <conti.nicolas@escola.pr.gov.br>
2025-06-06 09:54:21 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,64 @@
+
+criaCartao(
+    'Valorant',
+    'O que os controladores fazem',
+    'Os controladores são a classe mais importante de todo o jogo pois eles com suas smokes diminuem o risco de seus aliados tomarem abates e a não ficarem em mais perigo nessa classe é importante ter mais cautela e paciencia para jogar'
+)
+criaCartao(
+    'Valorant',
+    'Qual o mapa que a Raze mais se destaca?',
+    'Bind por ser um mapa com muitas lacunas e as skills da raze consegue limpar diversas dessas áreas e suas skils serem perfeitas para esse mapa'
+)
+criaCartao(
+    'Valorant',
+    'Qual o meta atual da bind',
+    'O meta da Bind é Raze, Chyper, Fade, Brinstone e o ultimo pode ser flex como uma Skye'
+)
+criaCartao(
+    'Valorant',
+    'O que os Duelistas fazem?',
+    'Os duelistas ganham espaço, para o time, e usam de seus recursos e de seu aliados para fazer disso, essa é uma classe bem agressiva'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual a função dos Iniciadores?',
+    'Os iniciadores são os responsaveis por plantar a spike e ganhar informação para o time com seus recursos, eles participam da linha de frente junto com os duelistas praticamente uma dupla inseparavel'
+)
+
+criaCartao(
+    'Valorant',
+    'O que os Sentinelas fazem?',
+    'Um sentinela é a base da defesa do time, ele defende costas com suas abilidades e ganha espaço sozinho, podendo ou não ir com o time assim ganhando informação se o tive deve ir para o outro bomb e cortando a rotação inimiga'
+)
+
+criaCartao(
+    'Valorant',
+    'O que os Controladores fazem?',
+    'Eles são responsaveis por diminuir a chance de tomar tiros por todos os lados sendo a função mais importante, é a classe mais importante de todas e que mais precisa de visão de jogo e noção para jogar'
+)
+criaCartao(
+    'Valorant',
+    'Quanto tempo demora pra spike explodir após ser plantada?',
+    'A spike demora 45 segundos para explodir após ser plantada, 7 segundos para defusar e mais ou menos 3 segundos para fazer metadinha'
+)
+criaCartao(
+    'Valorant',
+    'Por que as skins mudam minha gameplay?',
+    'Pois seu cérebro após comprar uma skin que vc tava namorando ele entende como uma recompensa e libera o hormonio do prazer, assim fazendo você jogar melhor por mais que a skin não mude nada praticamente no jogo'
+)
+criaCartao(
+    'Valorant',
+    'Como atirar igual um radiante?',
+    'Pra atirar igual um radiante você precisa fazer o famoso AD você anda para um lado aperta a tecla contrária para você parar o boneco atira e faz o inverso até você matar.'
+)
+criaCartao(
+    'Valorant',
+    'Como realmente melhorar no jogo?',
+    'Pense no o que você vai fazer, faça um plano veja como você pode sair daquela situação veja seus aliados no minimapa e comunique, a comunicação é a chave para um bom time'
+)
+criaCartao(
+    'Valorant',
+    'Como fazer economia certo?',
+    'A economia é variavel, porém há alguns rounds que da para ter um padrão como no primeiro round do half o pistol, nele você não quer economizar compre tudo, se você ganhar o pistol você deve comprar um escudo pesado e uma arma pesada'
+)
